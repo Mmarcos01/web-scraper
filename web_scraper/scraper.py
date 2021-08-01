9421ef335dba316dc82451b611312f6601d2474f
Marie <marie.marcos25@gmail.com>
2021-07-31 17:11:30 -0700

    citations returned

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -4,20 +4,19 @@ from bs4 import BeautifulSoup
 URL = 'https://en.wikipedia.org/wiki/History_of_Mexico'
 page = requests.get(URL)
 
-# print(page)
-# print(page.__dir__())
-
-soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup)
-
-# sup class="noprint Inline-Template Template-Fact"
-# results = soup.find(class_= '"Wikipedia:Citation needed"')
-citations = soup.find_all(class_= 'Template-Fact')
-print(citations)
-print(len(citations))
-
 def get_citations_needed_count(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
     citations = soup.find_all(class_= 'Template-Fact')
-    print(len(citations))
+    print (len(citations))
 
-def get_citations_needed_report():
+
+def get_citations_needed_report(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    citations = soup.find_all(class_= 'Template-Fact')
+    for cite in citations:
+        print(cite.parent.text)
+
+get_citations_needed_count(URL)
+get_citations_needed_report(URL)
